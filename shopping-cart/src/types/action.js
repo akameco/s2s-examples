db1e5a3d78d180660038117e28e05ed978a5e936
akameco <akameco.t@gmail.com>
2017-10-02 20:27:05 +0900

    Fix

--- shopping-cart/src/types/action.js
+++ shopping-cart/src/types/action.js
@@ -2,4 +2,11 @@
 import type { Action as CartContainerAction } from '../containers/CartContainer/actionTypes'
 import type { Action as ProductsContainerAction } from '../containers/ProductsContainer/actionTypes'
 
-export type Action = CartContainerAction | ProductsContainerAction
+export type ReduxInitAction = {
+  type: '@@INIT',
+}
+
+export type Action =
+  | ReduxInitAction
+  | CartContainerAction
+  | ProductsContainerAction
