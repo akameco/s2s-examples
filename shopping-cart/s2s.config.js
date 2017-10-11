1ef67ad80edb163374f99c6ed80f0888b0762e5c
akameco <akameco.t@gmail.com>
2017-10-12 00:08:49 +0900

    Use s2s-plugins-redux

--- shopping-cart/s2s.config.js
+++ shopping-cart/s2s.config.js
@@ -1,65 +1,9 @@
 // @flow
-const path = require('path')
-
-const cwd = process.cwd()
-const getRootDir = (...x) => path.resolve(cwd, 'src', ...x)
-const getTyepDir = x => getRootDir('types', x)
-const rootReducerPath = getRootDir('reducer.js')
-const rootActionPath = getTyepDir('action.js')
-const rootStatePath = getTyepDir('state.js')
+const plugins = require('s2s-plugins-redux')
 
 module.exports = {
   watch: './**/*.js',
-  plugins: [
-    {
-      test: /actionTypes.js$/,
-      plugin: ['s2s-action-types', { removePrefix: 'src/containers' }],
-    },
-    {
-      test: /actionTypes.js$/,
-      output: 'actions.js',
-      plugin: ['s2s-action-creater'],
-    },
-    {
-      test: /actionTypes.js$/,
-      input: 'reducer.js',
-      output: 'reducer.js',
-      plugin: ['s2s-reducer-case-creater'],
-    },
-    {
-      test: /actionTypes.js$/,
-      input: rootActionPath,
-      output: rootActionPath,
-      plugin: [
-        's2s-action-root',
-        { input: 'src/**/actionTypes.js', output: rootActionPath },
-      ],
-    },
-    {
-      test: /containers\/.+reducer.js/,
-      input: 'reducer.test.js',
-      output: 'reducer.test.js',
-      plugin: ['s2s-reducer-test-case'],
-    },
-    {
-      test: /containers\/.+reducer.js/,
-      input: rootStatePath,
-      output: rootStatePath,
-      plugin: [
-        's2s-state-root',
-        { input: 'src/containers/**/reducer.js', output: rootStatePath },
-      ],
-    },
-    {
-      test: /containers\/.+reducer.js/,
-      input: rootReducerPath,
-      output: rootReducerPath,
-      plugin: [
-        's2s-reducer-root',
-        { input: 'src/containers/**/reducer.js', output: rootReducerPath },
-      ],
-    },
-  ],
+  plugins,
   templates: [
     { test: /containers\/.*\/index.js/, input: 'containers.js' },
     { test: /components\/.*\/index.js/, input: 'components.js' },
